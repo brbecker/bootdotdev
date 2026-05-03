e9e5aa29a8b9c5cf675d77f1dc746cc6859aca20
Brian R. Becker <brbecker@gmail.com>
2026-05-02 17:00:30 -0700

    bookbot: Count words

--- bookbot/main.py
+++ bookbot/main.py
@@ -2,7 +2,12 @@ def get_book_text(path: str) -> str:
     with open(path) as f:
         return f.read()
 
+def count_words(text: str) -> int:
+    return len(text.split())
+
 def main() -> None:
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    words = count_words(text)
+    print(f"Found {words} total words")
 
 main()
