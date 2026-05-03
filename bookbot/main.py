1da838a94449475c1f71cd4131f5ca1137b10066
Brian R. Becker <brbecker@gmail.com>
2026-05-02 20:40:07 -0700

    bookbot: L5: Refactor

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,13 +1,12 @@
+from stats import get_num_words
+
 def get_book_text(path: str) -> str:
     with open(path) as f:
         return f.read()
 
-def count_words(text: str) -> int:
-    return len(text.split())
-
 def main() -> None:
     text = get_book_text("books/frankenstein.txt")
-    words = count_words(text)
+    words = get_num_words(text)
     print(f"Found {words} total words")
 
 main()
