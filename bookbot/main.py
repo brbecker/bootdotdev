01ce5a2943347344b3d573fd594b0644efbeddb2
Brian R. Becker <brbecker@gmail.com>
2026-05-02 21:02:42 -0700

    bookbot: L6: Count Characters

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_frequency
 
 def get_book_text(path: str) -> str:
     with open(path) as f:
@@ -8,5 +8,6 @@ def main() -> None:
     text = get_book_text("books/frankenstein.txt")
     words = get_num_words(text)
     print(f"Found {words} total words")
+    print(get_character_frequency(text))
 
 main()
--- bookbot/stats.py
+++ bookbot/stats.py
@@ -1,2 +1,8 @@
 def get_num_words(text: str) -> int:
     return len(text.split())
+
+def get_character_frequency(text: str) -> dict[str, int]:
+    freqs = {}
+    for c in text.lower():
+        freqs[c] = freqs.get(c, 0) + 1
+    return freqs
