e6bfa50c0d17234fbe62e76af6c4812e6e7bcbc3
Brian R. Becker <brbecker@gmail.com>
2026-05-14 12:09:06 -0700

    asteroids: CH4 L1: Asteroids

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
@@ -16,13 +18,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    # Create groups
-    updatable = pygame.sprite.Group()
+    # Create sprite groups
+    asteroids = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
 
     # Add classes to groups
-    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = (updatable,)
+    Player.containers = (drawable, updatable)
 
+    # Create initial objects
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
