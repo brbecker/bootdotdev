c75f86488efbd0c0b31ec9a217fb74943eccc05f
Brian R. Becker <brbecker@gmail.com>
2026-05-14 09:51:59 -0700

    asteroids: CH3 L5: Groups

--- asteroids/main.py
+++ asteroids/main.py
@@ -16,6 +16,13 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # Create groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Add classes to groups
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -25,10 +32,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for d in drawable:
+            d.draw(screen)
 
         pygame.display.flip()
 
