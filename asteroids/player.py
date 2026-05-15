981f113d63840b08830e9e465756ea1fbc2f7bdd
Brian R. Becker <brbecker@gmail.com>
2026-05-14 17:31:45 -0700

    asteroids: CH4 L4: Rate Limit

--- asteroids/player.py
+++ asteroids/player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -31,12 +32,17 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
+        if self.shot_cooldown > 0:
+            return
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(*self.position)
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         shot.velocity = rotated_vector * PLAYER_SHOOT_SPEED
 
     def update(self, dt):
+        self.shot_cooldown -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
