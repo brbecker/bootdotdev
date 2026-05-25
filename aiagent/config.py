f814f8b2fb745880bf40390c16d02f2f5a84bc61
Brian R. Becker <brbecker@gmail.com>
2026-05-25 16:10:42 -0700

    aiagent: CH4 L1: Agent Loop

--- aiagent/config.py
+++ aiagent/config.py
@@ -1,2 +1,5 @@
 # Maximum characters to return from a file
 MAX_CHARS=10000
+
+# Maximum calls to the LLM processing a prompt.
+MAX_LLM_STEPS=20
--- aiagent/main.py
+++ aiagent/main.py
@@ -1,11 +1,13 @@
 import argparse
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
 from call_function import available_functions, call_function
+from config import MAX_LLM_STEPS
 from prompts import system_prompt
 
 
@@ -15,6 +17,7 @@ def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    parser.add_argument("--debug", action="store_true", help="Enable debugging output")
     args = parser.parse_args()
 
     load_dotenv()
@@ -27,41 +30,67 @@ def main():
     ]
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        config=types.GenerateContentConfig(
-            system_instruction=system_prompt,
-            tools=[available_functions],
-        ),
-        contents=messages,
-    )
 
-    if response.usage_metadata is None:
-        raise RuntimeError("Gemini API call failed")
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    if response.function_calls is None:
-        print(response.text)
-    else:
-        function_results = []
-        for function_call in response.function_calls:
-            # print(f"Calling function: {function_call.name}({function_call.args})")
-            function_call_result = call_function(function_call)
+    for _ in range(MAX_LLM_STEPS):
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+        if args.debug:
+            print(f"Messages: {messages}")
 
-            fcr_parts = function_call_result.parts
-            if fcr_parts is None:
-                raise Exception("Error: function call result parts list is None")
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                system_instruction=system_prompt,
+                tools=[available_functions],
+            ),
+            contents=messages,
+        )
 
-            function_response = fcr_parts[0].function_response
-            if function_response is None:
-                raise Exception("Error: no function response found")
+        if response.usage_metadata is None:
+            raise RuntimeError("Gemini API call failed")
 
-            function_results.append(fcr_parts[0])
+        if args.verbose:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-            if args.verbose:
-                print(f"-> {function_response.response}")
+        if response.candidates:
+            for candidate in response.candidates:
+                content = candidate.content
+                if content:
+                    messages.append(content)
+                    if args.debug:
+                        print(f"- Adding response candidate to messages: {content}")
+
+        if response.function_calls is None:
+            print("Final response:")
+            print(response.text)
+            return
+        else:
+            function_responses = []
+            for function_call in response.function_calls:
+                # print(f"- Calling function: {function_call.name}({function_call.args})")
+                function_call_result = call_function(function_call, args.verbose)
+
+                fcr_parts = function_call_result.parts
+                if fcr_parts is None:
+                    raise Exception("Error: function call result parts list is None")
+
+                function_response = fcr_parts[0].function_response
+                if function_response is None:
+                    raise Exception("Error: no function response found")
+                if args.verbose:
+                    print(f"-> {function_response.response}")
+
+                function_responses.append(fcr_parts[0])
+
+            messages.append(types.Content(role="user", parts=function_responses))
+            if args.debug:
+                print(f"- Added function responses to messages: {messages[-1]}")
+
+    # If we reached a final solution, we would have returned from the loop. Print error
+    # message and exit with error code 1.
+    print(f"Error: Exceeded maximum of {MAX_LLM_STEPS} calls to the LLM.")
+    sys.exit(1)
 
 if __name__ == "__main__":
     main()
