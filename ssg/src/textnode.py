48f8d08b531889bdd87344bc1173aabde681043d
Brian R. Becker <brbecker@gmail.com>
2026-07-16 00:27:51 -0700

    ssg: CH2 L1: TextNode

---
+++ ssg/src/textnode.py
@@ -0,0 +1,35 @@
+from enum import Enum
+from types import NotImplementedType
+
+
+class TextType(Enum):
+    PLAIN_TEXT = "plain"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK_TEXT = "link"
+    IMAGE_TEXT = "image"
+
+
+class TextNode():
+    def __init__(
+            self,
+            text: str,
+            text_type: TextType,
+            url: str | None = None,
+            ) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other: object) -> bool | NotImplementedType:
+        if not isinstance(other, TextNode):
+            return NotImplemented
+        return (
+            self.text == other.text and
+            self.text_type == other.text_type and
+            self.url == other.url
+        )
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
