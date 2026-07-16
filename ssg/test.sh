5c678551692fccfce2a8d2d1c409c68a45b4a042
Brian R. Becker <brbecker@gmail.com>
2026-07-16 00:27:51 -0700

    ssg: CH2 L5: ParentNode

--- ssg/test.sh
+++ ssg/test.sh
@@ -1,4 +1,6 @@
 # python3 -m unittest discover -s src
 uv run --with coverage -m coverage run -m unittest discover -s src "$@"
+SAVE=$?
 uv run --with coverage -m coverage report
 uv run --with coverage -m coverage html
+exit $SAVE
